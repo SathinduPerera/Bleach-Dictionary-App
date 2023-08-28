336b3559fe7432604a2a6b6f571c85dda141ced5
Sathindu Perera <sathindu.geethika@gmail.com>
2023-08-28 19:24:04 +0530

    getting checkbox data from frontend

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,41 @@ import { useState, useEffect } from 'react'
 function App() {
   const [backend, setbackend] = useState([{}])
 
+  const [all, setAll] = useState(false);
+  const [captain, setCaptain] = useState(false);
+  const [lieutenant, setLieutenant] = useState(false);
+  const [formerCap, setFormerCap] = useState(false);
+  const [sub, setSub] = useState(false);
+  const [other, setother] = useState(false);
+
   // useEffect(() => {
   //   fetch("/api").then(
   //     (response) => response.json()
   //   ).then((data) => setbackend(data))
   // }, [])
+
+  useEffect(() => {
+    console.log(`
+    All : ${all}
+    Captain : ${captain}
+    Lieutenant : ${lieutenant}
+    Former Captain : ${formerCap}
+    Substitute Soul Reaper : ${sub}
+    Other : ${other}`)
+    const userdata = {
+      all : all, 
+      captain : captain, 
+      lieutenant : lieutenant,
+      formerCap : formerCap, 
+      sub : sub, 
+      other : other
+    }
+    fetch("api/post", {
+      method : "post",
+      headers : {"Content-Type" : "application/json"},
+      body: JSON.stringify(userdata)
+    }).then((response) => response.json()).then((data) => {console.log(data.message)})
+  }, [all, captain, formerCap, sub, other, lieutenant])
 
   useEffect(() => {
     fetch("/api").then(
@@ -22,10 +52,29 @@ function App() {
 
   return (
     <div>
+      <form>
+        <label htmlFor='all'>All Characters</label>
+        <input type='checkbox' id='all' onChange={() => setAll(!all)}></input>
+
+        <label htmlFor='Captain'>Captain</label>
+        <input type='checkbox' id='Captain' onChange={() => setCaptain(!captain)}></input>
+
+        <label htmlFor='lie'>Lieutenant</label>
+        <input type='checkbox' id='lie' onChange={() => setLieutenant(!lieutenant)}></input>
+
+        <label htmlFor='fc'>Former Captain</label>
+        <input type='checkbox' id='fc' onChange={() => setFormerCap(!formerCap)}></input>
+
+        <label htmlFor='sub'>Substitute Shinigami</label>
+        <input type='checkbox' id='sub' onChange={() => setSub(!sub)}></input>
+
+        <label htmlFor='other'>Other</label>
+        <input type='checkbox' id='other' onChange={() => setother(!other)}></input>
+      </form>
       <ol>
         {(typeof backend === 'undefined')? <p>Loading...</p> :
         backend.map((data, i) => (
-          <li id={i}>{data.Character} : {data.Sword}</li>
+          <li key={i}>{data.Character} : {data.Sword}</li>
         ))}
       </ol>
     </div>
--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
 import express from "express";
 import fs from "fs"
+import bodyParser from "body-parser"
+
+let getArr = []
 
 const app = express();
 
+app.use(bodyParser.json())
+
 let raw = fs.readFileSync("characters.json");
 let data = JSON.parse(raw);
-console.log(data)
+// data.forEach(element => {
+//     if()
+// });
+console.log(data[0])
 
 
 app.get("/api", (req, res) => {
     res.json(data)
+})
+
+app.post("/api/post", (req, res) => {
+    console.log(req.body.selections);
+    res.json({"message": "Options submitted"})
 })
 
 app.listen(5000, () => {
