9e7fff785c59338607a138c5d971c9719f5239a9
Sathindu Perera <sathindu.geethika@gmail.com>
2023-08-28 17:49:29 +0530

    get request handled and characters added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [backend, setbackend] = useState([{}])
+
+  // useEffect(() => {
+  //   fetch("/api").then(
+  //     (response) => response.json()
+  //   ).then((data) => setbackend(data))
+  // }, [])
+
+  useEffect(() => {
+    fetch("/api").then(
+      (response) => (response.json())
+    ).then(
+      (data) => {
+        setbackend(data)
+      }
+    )
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ol>
+        {(typeof backend === 'undefined')? <p>Loading...</p> :
+        backend.map((data, i) => (
+          <li id={i}>{data.Character} : {data.Sword}</li>
+        ))}
+      </ol>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
