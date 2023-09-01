8199e942759927ad58f0a1dc02a868dc6955783d
Sathindu Perera <sathindu.geethika@gmail.com>
2023-09-01 11:57:36 +0530

    sending checkbox data to backend

--- client/src/App.js
+++ client/src/App.js
@@ -4,41 +4,13 @@ import { useState, useEffect } from 'react'
 function App() {
   const [backend, setbackend] = useState([{}])
 
-  const [all, setAll] = useState(false);
+  const [all, setAll] = useState(true);
   const [captain, setCaptain] = useState(false);
   const [lieutenant, setLieutenant] = useState(false);
   const [formerCap, setFormerCap] = useState(false);
   const [sub, setSub] = useState(false);
   const [other, setother] = useState(false);
-
-  // useEffect(() => {
-  //   fetch("/api").then(
-  //     (response) => response.json()
-  //   ).then((data) => setbackend(data))
-  // }, [])
-
-  useEffect(() => {
-    console.log(`
-    All : ${all}
-    Captain : ${captain}
-    Lieutenant : ${lieutenant}
-    Former Captain : ${formerCap}
-    Substitute Soul Reaper : ${sub}
-    Other : ${other}`)
-    const userdata = {
-      all : all, 
-      captain : captain, 
-      lieutenant : lieutenant,
-      formerCap : formerCap, 
-      sub : sub, 
-      other : other
-    }
-    fetch("api/post", {
-      method : "post",
-      headers : {"Content-Type" : "application/json"},
-      body: JSON.stringify(userdata)
-    }).then((response) => response.json()).then((data) => {console.log(data.message)})
-  }, [all, captain, formerCap, sub, other, lieutenant])
+  const [sorted, setsorted] = useState(false);
 
   useEffect(() => {
     fetch("/api").then(
@@ -48,13 +20,96 @@ function App() {
         setbackend(data)
       }
     )
-  }, [])
+  }, [sorted])
+  
+
+  // useEffect(() => {
+
+  //   console.log(all, captain, lieutenant, formerCap, sub, other)
+    
+  //   if (all) {
+  //     backend.forEach(element => getArr.push(element))
+  //   } else {
+  //       if(other) {
+  //           let arr = filter("Other", backend)
+  //           arr.forEach(element => getArr.push(element))
+  //           console.log(arr)
+  //       }
+  //       if (captain) {
+  //           let arr = filter("Captain", backend)
+  //           arr.forEach(element => getArr.push(element))
+  //           console.log(arr)
+  //       }
+  //       if (lieutenant) {
+  //           let arr = filter("Lieutenant", backend)
+  //           arr.forEach(element => getArr.push(element))
+  //           console.log(arr)
+  //       }
+  //       if (formerCap) {
+  //           let arr = filter("Former Captain", backend)
+  //           arr.forEach(element => getArr.push(element))
+  //           console.log(arr)
+  //       }
+  //       if (sub) {
+  //           let arr = filter("Substitute Shinigami", backend)
+  //           arr.forEach(element => getArr.push(element))
+  //           console.log(arr)
+  //       }
+  //   }
+
+  //   console.log(getArr)
+  //   return getArr
+    
+  // }, [all, captain, formerCap, sub, other, lieutenant, backend, getArr])
+
+  function postSort(){
+    let arr = []
+    if (all) {
+          arr.push("All")
+        } else {
+            if(other) {
+              arr.push("Other")
+            }
+            if (captain) {
+              arr.push("Captain")
+            }
+            if (lieutenant) {
+              arr.push("Lieutenant")
+            }
+            if (formerCap) {
+              arr.push("Former Captain")
+            }
+            if (sub) {
+              arr.push("Substitute Shinigami")
+            }
+        }
+        
+        return arr
+  }
+
+  useEffect(() => {
+    let arr = postSort()
+    let userdata = {
+      selection_arr : arr
+    }
+    fetch("api/post", 
+    {
+      method: "post",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(userdata)
+    }).then((response) => response.json()).then((data) => console.log(data.message))
+  }, [captain, lieutenant, formerCap, all, other, sub])
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor='all'>All Characters</label>
-        <input type='checkbox' id='all' onChange={() => setAll(!all)}></input>
+        <input type='checkbox' id='all' onChange={() => setAll(!all)} defaultChecked></input>
 
         <label htmlFor='Captain'>Captain</label>
         <input type='checkbox' id='Captain' onChange={() => setCaptain(!captain)}></input>
@@ -70,11 +125,13 @@ function App() {
 
         <label htmlFor='other'>Other</label>
         <input type='checkbox' id='other' onChange={() => setother(!other)}></input>
+
+        <button onClick={() => {setsorted(!sorted)}}>Sort</button>
       </form>
       <ol>
         {(typeof backend === 'undefined')? <p>Loading...</p> :
         backend.map((data, i) => (
-          <li key={i}>{data.Character} : {data.Sword}</li>
+          <li key={i}>{data.Character} : {data.Sword} : {data.Position}</li>
         ))}
       </ol>
     </div>
--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import fs from "fs"
 import bodyParser from "body-parser"
 
-let getArr = []
 
 const app = express();
 
@@ -10,20 +9,120 @@ app.use(bodyParser.json())
 
 let raw = fs.readFileSync("characters.json");
 let data = JSON.parse(raw);
-// data.forEach(element => {
-//     if()
-// });
-console.log(data[0])
 
+function filter (position, arr) {
+    let returnArr= []
+    arr.forEach(element => {
+        if(element.Position === position){
+           returnArr.push(element)
+        }
+    });
+    return returnArr;
+}
+
+
+function sort() {
+
+    let getArr = []
+
+    if (all) {
+      backend.forEach(element => getArr.push(element))
+    } else {
+        if(other) {
+            let arr = filter("Other", backend)
+            arr.forEach(element => getArr.push(element))
+            console.log(arr)
+        }
+        if (captain) {
+            let arr = filter("Captain", backend)
+            arr.forEach(element => getArr.push(element))
+            console.log(arr)
+        }
+        if (lieutenant) {
+            let arr = filter("Lieutenant", backend)
+            arr.forEach(element => getArr.push(element))
+            console.log(arr)
+        }
+        if (formerCap) {
+            let arr = filter("Former Captain", backend)
+            arr.forEach(element => getArr.push(element))
+            console.log(arr)
+        }
+        if (sub) {
+            let arr = filter("Substitute Shinigami", backend)
+            arr.forEach(element => getArr.push(element))
+            console.log(arr)
+        }
+    }
+
+    console.log("sorting...")
+
+    return getArr
+
+  }
+
+
+//   function filter (position, arr) {
+//     let returnArr= []
+//     arr.forEach(element => {
+//         if(element.Position === position){
+//            returnArr.push(element)
+//         }
+//     });
+//     return returnArr;
+//   }
 
 app.get("/api", (req, res) => {
     res.json(data)
 })
 
+// app.post("/api/post", (req, res) => {
+//     let getArr = []
+//     let all = req.body.all
+//     let captain = req.body.captain
+//     let lieutenant = req.body.lieutenant
+//     let formerCap = req.body.formerCap
+//     let sub = req.body.sub
+//     let other = req.body.other
+
+//     console.log(all, captain, lieutenant, formerCap, sub, other)
+
+//     if (all) {
+//         getArr = data
+//     } else {
+//         if(other) {
+//             let arr = filter("Other", data)
+//             arr.forEach(element => getArr.push(element))
+//         }
+//         if (captain) {
+//             let arr = filter("Captain", data)
+//             arr.forEach(element => getArr.push(element))
+//         }
+//         if (lieutenant) {
+//             let arr = filter("Lieutenant", data)
+//             arr.forEach(element => getArr.push(element))
+//         }
+//         if (formerCap) {
+//             let arr = filter("Former Captain", data)
+//             arr.forEach(element => getArr.push(element))
+//         }
+//         if (sub) {
+//             let arr = filter("Substitute Shinigami", data)
+//             arr.forEach(element => getArr.push(element))
+//         }
+//     }
+
+//     console.log(getArr)
+
+//     fs.writeFileSync("Updated.json", JSON.stringify(getArr));
+
 app.post("/api/post", (req, res) => {
-    console.log(req.body.selections);
-    res.json({"message": "Options submitted"})
+    console.log(req.body.selection_arr)
+    res.json({"message" : "Form Submitted"})
 })
+
+    // res.json({"message" : "form Submitted"})
+// })
 
 app.listen(5000, () => {
     console.log("Server running in port 5000")
